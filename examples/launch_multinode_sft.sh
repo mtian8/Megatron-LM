9b590b06efca52e5bea0200379cc2133cb626b8b
Yufeng Du <yufengd4@gh-login02.delta.ncsa.illinois.edu>
2024-08-23 14:09:35 -0500

    rollback transformer engine

--- examples/launch_multinode_sft.sh
+++ examples/launch_multinode_sft.sh
@@ -8,9 +8,10 @@ echo $HOME
 FILE="$HOME/hostname.txt"
 echo "hostname text: $FILE"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+MEGATRON_DIR="$SCRIPT_DIR/.."
 rank=0
 #CONDA_COMMAND="conda activate mg-lm;"
-CONDA_COMMAND=""
+CONDA_COMMAND=":"
 # Function to perform SSH and run the command in the background
 
 ssh_command() {
@@ -19,8 +20,8 @@ ssh_command() {
 
     echo "Attempting to SSH into $line"
 
-    ssh -o ConnectTimeout=5 "$line" "source $HOME/.bashrc; export HOSTNAME_FILE=$FILE; $CONDA_COMMAND; cd $SCRIPT_DIR; bash ./run_sft_distributed.sh $rank" < /dev/null
-
+    # ssh -o ConnectTimeout=5 "$line" "apptainer exec --nv --bind /projects /sw/user/NGC_containers/pytorch_24.07-py3.sif bash -c 'export HOSTNAME_FILE=$FILE; $CONDA_COMMAND; cd $MEGATRON_DIR; echo '\''hello'\''; python -c '\''import torch; print(torch.cuda.is_available())'\'';'" < /dev/null #bash ./tools/run_sft_distributed.sh $rank" < /dev/null
+    ssh -o ConnectTimeout=5 "$line" "apptainer exec --nv --bind /projects /sw/user/NGC_containers/pytorch_24.07-py3.sif bash -c 'export HOSTNAME_FILE=$FILE; $CONDA_COMMAND; cd $MEGATRON_DIR; bash ./examples/run_sft_distributed.sh $rank'" < /dev/null
     if [ $? -eq 0 ]; then
 
         echo "SSH to $line successful"
@@ -31,7 +32,7 @@ ssh_command() {
 
     fi
 
-    #(( rank += 1))
+    (( rank += 1))
 
 }
 
--- examples/run_sft_distributed.sh
+++ examples/run_sft_distributed.sh
@@ -3,21 +3,25 @@ set -ex
 
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
-export GPUS_PER_NODE=8
+export GPUS_PER_NODE=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 # Change for multinode config
-PBSNODEFILE=/home/mtian8/pbs_nodefile
+PBSNODEFILE=$HOME/hostname.txt
 export MASTER_ADDR=$(head -n 1 $PBSNODEFILE)
 export MASTER_PORT=6000
 NNODES=`wc -l < $PBSNODEFILE`
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 NODEID=$1 #RANDOM
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+MEGATRON_DIR="$SCRIPT_DIR/.."
 
 # torchrun --nproc_per_node=$GPUS_PER_NODE --nnodes=$NNODES --node_rank=$PALS_NODEID --master_addr=$MASTER_ADDR \
 #        --master_port=$MASTER_PORT test_multinode.py --backend=nccl
-{(CUDA_DEVICE_MAX_CONNECTIONS=1  torchrun \
+echo "In script: run_sft_distributed for rank $NODEID"
+
+CUDA_DEVICE_MAX_CONNECTIONS=1  torchrun \
 --nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODEID \
 --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
-Megatron-LM/sft_gpt.py --log-interval 1 --recompute-granularity full --recompute-method uniform --recompute-num-layers 4 \
+$MEGATRON_DIR/sft_gpt.py --log-interval 1 --recompute-granularity full --recompute-method uniform --recompute-num-layers 4 \
 --tensor-model-parallel-size 8 --pipeline-model-parallel-size 1 \
 --use-checkpoint-args --no-load-optim --no-load-rng \
 --seq-length 32 --max-position-embeddings 32 --rotary-base 8000000 \
@@ -27,16 +31,16 @@ Megatron-LM/sft_gpt.py --log-interval 1 --recompute-granularity full --recompute
 --norm-epsilon 1e-5 --no-masked-softmax-fusion \
 --optimizer adam --adam-beta1 0.9 --adam-beta2 0.999 --adam-eps 1e-08 \
 --exit-on-missing-checkpoint --dataloader-type cyclic \
---data-path /home/mtian8/mtian8/tpc/mtian8/data/slimpajama_packed_64k_5b_per_source_down_sample_0.1 \
+--data-path $MEGATRON_DIR/../datasets/slimpajama-per-source-length-upsample \
 --save-interval 1 --eval-interval 500  --window-size 4096 0 \
---eval-iters 10 --tokenizer-type Llama3Tokenizer --attention-softmax-in-fp32 \
+--eval-iters 10 --tokenizer-type Llama2Tokenizer --attention-softmax-in-fp32 \
 --untie-embeddings-and-output-weights --normalization RMSNorm --position-embedding-type rope \
---tokenizer-model /home/mtian8/mtian8/tpc/mtian8/model/HF_model/Meta-Llama-3-8B/original/tokenizer.model \
---load /home/mtian8/mtian8/tpc/mtian8/model/MG_model/llama3_8B_tp_8_pp_1 \
---save /home/mtian8/mtian8/tpc/mtian8/data/checkpoint_long_sliding_window_4k \
---tensorboard-dir 'Megatron-LM/log' --tensorboard-queue-size 5 --wandb-project 'llama3_8B_sliding_window_4k ' \
---wandb-exp-name 'llama3_8B_sliding_window_4k ' --wandb-save-dir '/home/mtian8/mtian8/tpc/mtian8/wandb' \
-2>&1 | tee out.out )&}
+--tokenizer-model $MEGATRON_DIR/../models/Llama-2-7b-hf/tokenizer.model \
+--load $MEGATRON_DIR/../models/MG_llama2_7B_tp_4_pp_2 \
+--save $MEGATRON_DIR/../models/checkpoint_long_sliding_window_4k \
+--tensorboard-dir "$MEGATRON_DIR/log" --tensorboard-queue-size 5 --wandb-project 'llama2_7B_sliding_window_4k ' \
+--wandb-exp-name 'llama2_7B_sliding_window_4k ' --wandb-save-dir "$MEGATRON_DIR/../wandb" \
+2>&1 | tee out.out 
 
 
 echo "HELLO"
