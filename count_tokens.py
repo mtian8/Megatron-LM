dc5cfd6f9e58efe975a5ec8f6302566208a12743
Minyang Tian <mtian8@gh-login02.delta.ncsa.illinois.edu>
2024-10-25 19:11:11 -0500

    add some util scripts

---
+++ count_tokens.py
@@ -0,0 +1,10 @@
+from megatron.core.datasets import indexed_dataset
+import numpy as np
+
+idx_file = '/u/mtian8/LLM/data/software/software_text_document.idx'
+IR = indexed_dataset._IndexReader(idx_file,False)
+sum = 0
+for i in range(IR.__len__()):
+    sum +=IR.__getitem__(i)[1]
+print(sum)
+    
